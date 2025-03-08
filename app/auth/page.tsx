73d3796866a8fb9fd756c446846356dd6918eae4
vikrant <vikrantzone123@gmail.com>
2025-03-09 00:40:28 +0530

    neee

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,18 +1,18 @@
-import { SignInForm } from "@/components/ui/signin-form"
-import { SignupForm } from "@/components/ui/signup-form"
+import { SignInForm } from "@/components/ui/signin-form";
+import { SignupForm } from "@/components/ui/signup-form";
 
 export default function AuthPage() {
   return (
     <div className="container mx-auto py-10 px-4">
-      <div className="sm:flex  md:flex lg:flex w-full justify-center mx-auto">
+      <div className="flex flex-col sm:flex-row md:flex-row lg:flex-row w-full justify-center mx-auto gap-6">
         <div className="w-full lg:w-[30%]">
           <SignInForm />
         </div>
+        <div className="h-px w-full bg-border sm:hidden" />
         <div className="w-full lg:w-[70%]">
           <SignupForm />
         </div>
       </div>
     </div>
-  )
+  );
 }
-
